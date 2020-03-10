e070ca1b7c6b4a6f886facbef6d6de4fba7c0d2d
bliff182 <michael.bither@gmail.com>
2020-03-10 10:05:47 -0400

    projects added

--- src/resumeData.js
+++ src/resumeData.js
@@ -118,16 +118,71 @@ let resumeData = {
       projurl: "https://github.com/bliff182/movision"
     },
     {
-      name: "Onion Peeler",
-      description: "View and comment on the latest articles from The Onion",
-      imgurl: "images/portfolio/o_s-home.png",
-      projurl: "https://github.com/bliff182/banger"
+      name: "LIRI",
+      description:
+        "Node CLI app for discovering concert, song, and movie information",
+      imgurl: "images/portfolio/liri.png",
+      projurl: "https://github.com/bliff182/scrollax"
     },
     {
       name: "Bamazon",
-      description: "Node CLI app for making purchases and updating inventory",
+      description: "Node CLI app for making purchases and updating inventory.",
       imgurl: "images/portfolio/BC_initial.png",
       projurl: "https://github.com/bliff182/micate.js"
+    },
+    {
+      name: "Batman Hangman",
+      description: "How well do you know your Batman characters?",
+      imgurl: "images/portfolio/batman.png",
+      projurl: "https://github.com/bliff182/barsel"
+    },
+    {
+      name: "Crystal Collector",
+      description: "Try to match the target score!",
+      imgurl: "images/portfolio/crystal.png",
+      projurl: "https://github.com/bliff182/bookish-bassoon"
+    },
+    {
+      name: "Gridiron Report",
+      description: "Stay up to date with your favorite NFL teams.",
+      imgurl: "images/portfolio/gridiron.png",
+      projurl: "https://github.com/bliff182/congenial-meme"
+    },
+    // {
+    //   name: "Eat-Da-Burger!",
+    //   description: "Input burgers you'd like to eat, then devour!",
+    //   imgurl: "images/portfolio/burger.png",
+    //   projurl: "https://github.com/bliff182/mulberwaul"
+    // },
+    // {
+    //   name: "Friend Finder",
+    //   description: "Find your best match!",
+    //   imgurl: "images/portfolio/friendfinder.png",
+    //   projurl: "https://github.com/bliff182/modplugin"
+    // },
+    // {
+    //   name: "GifTastic Movies!",
+    //   description: "Search for gifs of your favorite movies.",
+    //   imgurl: "images/portfolio/giftastic.png",
+    //   projurl: "https://github.com/bliff182/filterer"
+    // },
+    {
+      name: "Seinfeld Trivia",
+      description: "How well do you know Seinfeld?",
+      imgurl: "images/portfolio/seinfeld.png",
+      projurl: "https://github.com/bliff182/shareforx"
+    },
+    {
+      name: "PlanIt!",
+      description: "Create and maintain lists of all kinds.",
+      imgurl: "images/portfolio/planit.png",
+      projurl: "https://github.com/bliff182/vallum"
+    },
+    {
+      name: "Onion Peeler",
+      description: "View and comment on the latest articles from The Onion.",
+      imgurl: "images/portfolio/onion.png",
+      projurl: "https://github.com/bliff182/banger"
     }
   ],
   testimonials: [
