08e9dbfc922f87bf273cb340be10ac9fc82a8f27
bliff182 <michael.bither@gmail.com>
2020-03-10 06:51:20 -0400

    Updates

---
+++ precache-manifest.93889416e1e158dd0b5e360497dfdc6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf38b5606ccb2e55b5a1",
+    "url": "/portfolio/static/css/main.08a9894e.chunk.css"
+  },
+  {
+    "revision": "cf38b5606ccb2e55b5a1",
+    "url": "/portfolio/static/js/main.cf38b560.chunk.js"
+  },
+  {
+    "revision": "66bbb3f212da5d675074",
+    "url": "/portfolio/static/js/1.66bbb3f2.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "28f65d2ac0d37e6e09f89e32cb929b2a",
+    "url": "/portfolio/index.html"
+  }
+];
